b161fe4dc8924e3d7a77039bdfe27fea37b5c2b7
DevTec2020 <leonardo.silva.inf@gmail.com>
2024-12-27 23:51:37 -0300

    Adicionando tabelas no banco com a migrate

--- knexfile.ts
+++ knexfile.ts
@@ -5,6 +5,12 @@ export default {
         filename: "./src/database/database.db", 
     }, 
 
+    // Protege as chaves do banco e faz com que só possa ser criado produtos que tenham relação com as keys das tabelas.
+    pool: {
+        afterCreate: (connection: any, done: any) => {
+        done()
+        },
+    },
     useNullAsDefault: true, 
 
     migrations: { 
